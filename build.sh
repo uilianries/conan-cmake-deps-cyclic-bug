4e630ebd8207bea1fd0bbe2de15e223154273237
Uilian Ries <uilianries@gmail.com>
2024-08-06 15:32:59 +0200

    improve steps
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-echo "****** BUILD PROJECT USING CMAKE DEPS *********"
-conan create .
+
+echo "****** BUILD PROJECT *********"
+conan create . -tf=""
 
 printf "\n===================================================================\n"
 
-echo "****** BUILD PROJECT USING PKG CONFIG DEPS *********"
-conan create . -c tools.cmake.cmaketoolchain:extra_variables='{"USE_PKG_CONFIG": "OFF"}'
+echo "****** TEST PROJECT USING CMAKE DEPS *********"
+conan test test_package foo/0.1.0
+
+printf "\n===================================================================\n"
+
+echo "****** TEST PROJECT USING PKG CONFIG DEPS *********"
+conan test test_package foo/0.1.0 -c tools.cmake.cmaketoolchain:extra_variables='{"USE_PKG_CONFIG": "ON"}'
